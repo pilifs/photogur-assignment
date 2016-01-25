7a3a4815660e4c06182c6685e4fd1949706814d9
Filip Stankovic <filips@gmail.com>
2016-01-25 18:44:22 -0500

    Complete second app, forgot to commit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,47 @@
 class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  # Why is this necessary? --Perhaps just to initialize form, does not save... Would it work with required params? How to do req'd params?
+  def new
+    @photo = Photo.new
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    if @photo.update_attributes(photo_params)
+      redirect_to("/photos/#{@photo.id}")
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to photos_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    redirect_to photos_url
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:author, :title, :url)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Photo.create!(
+  :title  => "The old church on the coast of White sea",
+  :author => "Sergey Ershov",
+  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+)
+
+Photo.create!(
+  :title  => "Sea Power",
+  :author => "Stephen Scullion",
+  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+)
+
+Photo.create!(
+  :title  => "Into the Poppies",
+  :author => "John Wilhelm",
+  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+)
